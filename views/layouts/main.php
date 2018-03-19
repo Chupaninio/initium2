20f9680d65d0751d19331ccf8ecd4a837cf07e8e
Chupaninio <andrei_chupeev@mail.ru>
2018-03-20 01:24:06 +0300

    шрщш

---
+++ views/layouts/main.php
@@ -0,0 +1,95 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<div id = "toTop" > <span class="glyphicon glyphicon-circle-arrow-up"></span> </div>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Главная', 'url' => ['/site/index']],
+            ['label' => 'Услуги', 'url' => ['/site/about']],
+            ['label' => 'Порфтолио', 'url' => ['/site/about']],
+            ['label' => 'Контакты', 'url' => ['/site/contact']],
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container-fluid">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; INITIUM <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
+<script>
+    $(function() {
+
+        $(window).scroll(function() {
+
+            if($(this).scrollTop() != 0) {
+
+                $('#toTop').fadeIn();
+
+            } else {
+
+                $('#toTop').fadeOut();
+
+            }
+
+        });
+
+        $('#toTop').click(function() {
+
+            $('body,html').animate({scrollTop:0},800);
+
+        });
+    });
+</script>
---
+++ views/site/index.php
@@ -0,0 +1,82 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+    <div class="container-fluid" style="height: 100vh; background-image: url('https://www.wylab.net/wp-content/uploads/2017/04/informatica.jpg'); color: #ffffff">
+<!--        <h1 style="text-align: center; font-family: 'Cormorant Garamond', serif; font-size: 100px">INITIUM</h1>-->
+<!--        <h2 style="text-align: center; font-family: 'Cormorant Garamond', serif; font-size: 80px"> ДАДИМ НАЧАЛО ВАШЕЙ</h2>-->
+    </div>
+    <div class="container-fluid" style="background: linear-gradient(to right, #412cad, #db31cf);color: #5ff9fc"> <h1 style="text-align: center; font-size: 40px;font-family: 'PT Sans Caption', sans-serif;">НАШИ УСЛУГИ</h1></div>
+    <div class="container-fluid" style="height: 90vh">
+        <div class="grid">
+            <figure class="effect-julia">
+                <img src="../images/landing.jpg" alt="img21"/>
+                <figcaption>
+                    <h2>Landing <span>page</span></h2>
+                    <div>
+                        <p>Julia dances in the deep dark</p>
+                        <p>She loves the smell of the ocean</p>
+                        <p>And dives into the morning light</p>
+                        <p>And dives into the morning light</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>
+            </figure>
+            <figure class="effect-julia">
+                <img src="../images/visitka.jpg" alt="img22"/>
+                <figcaption>
+                    <h2>Сайт <span>визитка</span></h2>
+                    <div>
+                        <p>Julia dances in the deep dark</p>
+                        <p>She loves the smell of the ocean</p>
+                        <p>And dives into the morning light</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>
+            </figure>
+            <figure class="effect-julia">
+                <img src="../images/e-shop.jpg" alt="img22"/>
+                <figcaption>
+                    <h2>Интернет <span>магазин</span></h2>
+                    <div>
+                        <p>Julia dances in the deep dark</p>
+                        <p>She loves the smell of the ocean</p>
+                        <p>And dives into the morning light</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>
+            </figure>
+            <figure class="effect-julia">
+                <img src="img/22.jpg" alt="img22"/>
+                <figcaption>
+                    <h2>Passionate <span>Julia</span></h2>
+                    <div>
+                        <p>Julia dances in the deep dark</p>
+                        <p>She loves the smell of the ocean</p>
+                        <p>And dives into the morning light</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>
+            </figure>
+        </div>
+    </div>
+    <div class="container-fluid" style="background: linear-gradient(to right, #412cad, #db31cf);color: #5ff9fc"> <h1 style="text-align: center; font-size: 40px;font-family: 'PT Sans Caption', sans-serif;">ПОРТФОЛИО</h1></div>
+    <div class="container-fluid" style="height: 90vh;">
+
+    </div>
+    <div class="container-fluid" style="background: linear-gradient(to right, #412cad, #db31cf);color: #5ff9fc"> <h1 style="text-align: center; font-size: 40px;font-family: 'PT Sans Caption', sans-serif;">О НАС</h1></div>
+    <div class="container-fluid" style="height: 90vh;">
+
+    </div>
+    <div class="container-fluid" style="background: linear-gradient(to right, #412cad, #db31cf);color: #5ff9fc"> <h1 style="text-align: center; font-size: 40px;font-family: 'PT Sans Caption', sans-serif;">ПОЧЕМУ МЫ?</h1></div>
+    <div class="container-fluid" style="height: 90vh;">
+
+    </div>
+    <div class="container-fluid" style="background: linear-gradient(to right, #412cad, #db31cf);color: #5ff9fc"> <h1 style="text-align: center; font-size: 40px;font-family: 'PT Sans Caption', sans-serif;">КОНТАКТЫ</h1></div>
+    <div class="container-fluid" style="height: 90vh;">
+
+    </div>
+</div>
