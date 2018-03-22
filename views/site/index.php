002e9b09c81034f79975e4e56c5353093e9376c8
Chupaninio <andrei_chupeev@mail.ru>
2018-03-23 01:16:10 +0300

    test

--- views/site/index.php
+++ views/site/index.php
@@ -2,15 +2,19 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\captcha\Captcha;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
     <div class="container-fluid" style="height: 100vh; background-image: url('https://www.wylab.net/wp-content/uploads/2017/04/informatica.jpg'); color: #ffffff">
-<!--        <h1 style="text-align: center; font-family: 'Cormorant Garamond', serif; font-size: 100px">INITIUM</h1>-->
-<!--        <h2 style="text-align: center; font-family: 'Cormorant Garamond', serif; font-size: 80px"> ДАДИМ НАЧАЛО ВАШЕЙ</h2>-->
+        <h1 style="text-align: center; font-family: 'Cormorant Garamond', serif; font-size: 100px">INITIUM</h1>
+        <h2 style="text-align: center; font-family: 'Cormorant Garamond', serif; font-size: 65px"> НАЧАЛО ВАШЕГО ПУТИ В ВЕБ-ПРОСТРАНСТВЕ </h2>
     </div>
     <div class="container-fluid" style="background: linear-gradient(to right, #412cad, #db31cf);color: #5ff9fc"> <h1 style="text-align: center; font-size: 40px;font-family: 'PT Sans Caption', sans-serif;">НАШИ УСЛУГИ</h1></div>
-    <div class="container-fluid" style="height: 90vh">
+    <div class="container-fluid">
         <div class="grid">
             <figure class="effect-julia">
                 <img src="../images/landing.jpg" alt="img21"/>
@@ -77,7 +81,61 @@ $this->title = 'My Yii Application';
 
     </div>
     <div class="container-fluid" style="background: linear-gradient(to right, #412cad, #db31cf);color: #5ff9fc"> <h1 style="text-align: center; font-size: 40px;font-family: 'PT Sans Caption', sans-serif;">КОНТАКТЫ</h1></div>
-    <div class="container-fluid" style="height: 90vh;">
+    <div class="container-fluid">
+        <div class="site-contact">
+            <h1><?= Html::encode($this->title) ?></h1>
 
+            <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+
+                <div class="alert alert-success">
+                    Thank you for contacting us. We will respond to you as soon as possible.
+                </div>
+
+                <p>
+                    Note that if you turn on the Yii debugger, you should be able
+                    to view the mail message on the mail panel of the debugger.
+                    <?php if (Yii::$app->mailer->useFileTransport): ?>
+                        Because the application is in development mode, the email is not sent but saved as
+                        a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
+                                                                                                            Please configure the <code>useFileTransport</code> property of the <code>mail</code>
+                        application component to be false to enable email sending.
+                    <?php endif; ?>
+                </p>
+
+            <?php else: ?>
+
+                <p>
+                    If you have business inquiries or other questions, please fill out the following form to contact us.
+                    Thank you.
+                </p>
+
+                <div class="row">
+                    <div class="col-lg-5">
+
+                        <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+
+                        <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+
+                        <?= $form->field($model, 'email') ?>
+
+                        <?= $form->field($model, 'subject') ?>
+
+                        <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+
+                        <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                            'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                        ]) ?>
+
+                        <div class="form-group">
+                            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                        </div>
+
+                        <?php ActiveForm::end(); ?>
+
+                    </div>
+                </div>
+
+            <?php endif; ?>
+        </div>
     </div>
 </div>
